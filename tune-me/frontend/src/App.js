91f622002c71a0cb86b78be21aa89d818d218694
Zaina <Zaina@DESKTOP-EI3LB00>
2023-04-05 04:05:57 -0400

    Updated app to accept more parameters

--- tune-me/frontend/src/App.js
+++ tune-me/frontend/src/App.js
@@ -6,7 +6,12 @@ function App() {
   const [inputValue, setInputValue] = useState('');
   const [artist, setArtist] = useState('');
   const [year, setYear] = useState('');
+  const [genres, setGenres] = useState([]);
+  const [themes, setThemes] = useState([]);
+  const [advanced, setAdvancedSearch] = useState({});
+  
   const [songs, setSongs] = useState([]);
+  
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -20,12 +25,27 @@ function App() {
     setYear(event.target.value)
   }
 
+  const handleGenresChange = (event) =>{
+    setGenres(event.target.value)
+  }
+
+  const handleThemesChange = (event) =>{
+    setThemes(event.target.value)
+  }
+
+  const handleAdvancedChange = (event) =>{
+    setAdvancedSearch(event.target.value)
+  }
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     var bodyFormData = new FormData();
     bodyFormData.append('song', inputValue);
     bodyFormData.append('artist', artist)
     bodyFormData.append('year',year)
+    bodyFormData.append('genres',genres)
+    bodyFormData.append('themes',themes)
+    bodyFormData.append('advanced',advanced)
     axios({
       method: "post",
       url: "http://localhost:5000/songs",
@@ -66,10 +86,37 @@ function App() {
           <label htmlFor="year">Enter year:</label>
           <input
             id="year"
-            type="text"
+            type="number"
+            min="1900" 
+            max="2099" 
+            step="1"
             value={year}
             onChange={handleYearChange}
           />
+
+          <label htmlFor="genres">Enter Genres:</label>
+          <input
+            id="genres"
+            type="text"
+            value={genres}
+            onChange={handleThemesChange}
+          />
+
+          <label htmlFor="themes">Enter themes:</label>
+          <input
+            id="themes"
+            type="text"
+            value={themes}
+            onChange={handleThemesChange}
+          />
+          <label htmlFor="advanced">Advanced Search Options:</label>
+          <input
+            id="advanced"
+            type="text"
+            value={advanced}
+            onChange={handleAdvancedChange}
+          />
+
           <button type="submit">Search</button>
         </form>
         <h2>Recommended Songs</h2>
